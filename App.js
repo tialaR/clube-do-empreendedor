ee829ffaf4e5c430d4f27c0ae9afceb2cd8ae031
Tiala Fernanda de Santana Rocha <tiala.rocha@pampa.compasso>
2022-06-18 15:31:17 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,25 @@
+
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Clube do empreendedor</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+});
+
+export default App;

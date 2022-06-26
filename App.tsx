03ed27fcab631f215ff27d0dadcc76fe396665f4
Tiala Fernanda de Santana Rocha <tiala.rocha@pampa.compasso>
2022-06-26 16:43:00 -0300

    Add status bar color

--- App.tsx
+++ App.tsx
@@ -1,18 +1,26 @@
 
 import React from 'react';
+import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
 import Routes from './src/routes';
 
 import AppProvider from './src/hooks';
+import { colors } from './src/styles/colors';
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <AppProvider>
-        <Routes />
-      </AppProvider>
-    </NavigationContainer>
+    <>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor={colors.indigoA200}
+      />
+      <NavigationContainer>
+        <AppProvider>
+          <Routes />
+        </AppProvider>
+      </NavigationContainer>
+    </>
   );
 };
 

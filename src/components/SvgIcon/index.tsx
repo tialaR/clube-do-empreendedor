1588377b1ea9d36f9dee7586115e4cb291add08c
Tiala Fernanda de Santana Rocha <tiala.rocha@pampa.compasso>
2022-07-02 23:15:16 -0300

    Add Svgs in SvgIcon component

--- src/components/SvgIcon/index.tsx
+++ src/components/SvgIcon/index.tsx
@@ -4,9 +4,42 @@ import {SvgProps} from 'react-native-svg';
 import WhatsappSvg from '../../assets/svgs/whatsapp.svg';
 import CircleCheckSvg from '../../assets/svgs/circleCheck.svg';
 import HomeOutlineSvg from '../../assets/svgs/homeOutline.svg';
+import UserCircleSvg from '../../assets/svgs/userCircle.svg';
+import NotificationOutlineSvg from '../../assets/svgs/notificationOutline.svg';
+import NotificationDeactivatedSvg from '../../assets/svgs/notificationDeactivated.svg';
+import CaretDownSvg from '../../assets/svgs/caretDown.svg';
+import CaretUpSvg from '../../assets/svgs/caretUp.svg';
+import ChevronLeftSvg from '../../assets/svgs/chevronLeft.svg';
+import ImageSvg from '../../assets/svgs/image.svg';
+import LocationSvg from '../../assets/svgs/location.svg';
+import CheckedSquareSvg from '../../assets/svgs/checkedSquare.svg';
+import CupomSvg from '../../assets/svgs/cupom.svg';
+import DiscountSvg from '../../assets/svgs/discount.svg';
+import FacebookSvg from '../../assets/svgs/facebook.svg';
+import InstagramSvg from '../../assets/svgs/instagram.svg';
+import QrCodeSvg from '../../assets/svgs/qrCode.svg';
+import SearchSvg from '../../assets/svgs/search.svg';
 
 interface Props extends SvgProps {
-  name: 'whatsapp' | 'circleCheck' | 'homeOutline';
+  name:
+    | 'whatsapp'
+    | 'circleCheck'
+    | 'homeOutline'
+    | 'userCircle'
+    | 'notificationOutline'
+    | 'notificationDeactivated'
+    | 'caretDown'
+    | 'caretUp'
+    | 'chevronLeft'
+    | 'image'
+    | 'location'
+    | 'checkedSquare'
+    | 'cupom'
+    | 'discount'
+    | 'facebook'
+    | 'instagram'
+    | 'qrCode'
+    | 'search';
 }
 const SvgIcon: React.FC<Props> = ({name, ...rest}) => {
   const icon = useMemo((): ReactElement | undefined => {
@@ -20,6 +53,66 @@ const SvgIcon: React.FC<Props> = ({name, ...rest}) => {
 
     if (name === 'homeOutline') {
       return <HomeOutlineSvg {...rest} />;
+    }
+
+    if (name === 'userCircle') {
+      return <UserCircleSvg {...rest} />;
+    }
+
+    if (name === 'notificationOutline') {
+      return <NotificationOutlineSvg {...rest} />;
+    }
+
+    if (name === 'notificationDeactivated') {
+      return <NotificationDeactivatedSvg {...rest} />;
+    }
+
+    if (name === 'caretDown') {
+      return <CaretDownSvg {...rest} />;
+    }
+
+    if (name === 'caretUp') {
+      return <CaretUpSvg {...rest} />;
+    }
+
+    if (name === 'chevronLeft') {
+      return <ChevronLeftSvg {...rest} />;
+    }
+
+    if (name === 'image') {
+      return <ImageSvg {...rest} />;
+    }
+
+    if (name === 'location') {
+      return <LocationSvg {...rest} />;
+    }
+
+    if (name === 'checkedSquare') {
+      return <CheckedSquareSvg {...rest} />;
+    }
+
+    if (name === 'cupom') {
+      return <CupomSvg {...rest} />;
+    }
+
+    if (name === 'discount') {
+      return <DiscountSvg {...rest} />;
+    }
+
+    if (name === 'facebook') {
+      return <FacebookSvg {...rest} />;
+    }
+
+    if (name === 'instagram') {
+      return <InstagramSvg {...rest} />;
+    }
+
+    if (name === 'qrCode') {
+      return <QrCodeSvg {...rest} />;
+    }
+
+    if (name === 'search') {
+      return <SearchSvg {...rest} />;
     }
   }, [name, rest]);
 

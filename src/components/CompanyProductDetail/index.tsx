f4a26b3842a577e2085aac0b4de8c9b703ad5acb
Tiala Fernanda de Santana Rocha <tiala.rocha@pampa.compasso>
2022-07-05 13:50:49 -0300

    Add gradient loading at CompanyProductdetail component

--- src/components/CompanyProductDetail/index.tsx
+++ src/components/CompanyProductDetail/index.tsx
@@ -1,5 +1,10 @@
-import React from 'react';
+import React, {useMemo} from 'react';
 
+import {
+  BoxSkeletonLoading,
+  SpacingY,
+  TextsSkeletonLoading,
+} from '../../styles/globalStyles';
 import {
   Container,
   ProductContainer,
@@ -18,6 +23,45 @@ import {
   FooterContainer,
 } from './styles';
 
+const renderTitleLoading = () => (
+  <>
+    <SpacingY tiny />
+    <TextsSkeletonLoading width={180} />
+  </>
+);
+const TitleLoading = () => renderTitleLoading();
+
+const renderImageLoading = () => (
+  <>
+    <SpacingY medium />
+    <BoxSkeletonLoading width={160} height={160} />
+    <SpacingY tiny />
+  </>
+);
+const ImageLoading = () => renderImageLoading();
+
+const renderDescriptionsLoading = () => (
+  <>
+    <TextsSkeletonLoading width={150} thin />
+    <SpacingY tiny />
+    <TextsSkeletonLoading width={150} thin />
+    <SpacingY tiny />
+    <TextsSkeletonLoading width={150} thin />
+    <SpacingY tiny />
+    <TextsSkeletonLoading width={150} thin />
+  </>
+);
+const DescriptionsLoading = () => renderDescriptionsLoading();
+
+const renderQrCodeLoading = () => (
+  <>
+    <SpacingY small />
+    <BoxSkeletonLoading width={80} height={80} />
+    <SpacingY tiny />
+  </>
+);
+const QrCodeLoading = () => renderQrCodeLoading();
+
 export type Product = {
   id: string;
   name: string;
@@ -34,29 +78,54 @@ type Props = {
 };
 
 const CompanyProductDetail: React.FC<Props> = ({product}) => {
+  const isDescriptionLoaded = useMemo(
+    () =>
+      product?.price ||
+      product?.installment ||
+      product?.promotion ||
+      product?.soldBy,
+    [product],
+  );
   return (
     <Container>
       <ProductContainer>
         <ProductContainerContents>
-          <Name>{product?.name}</Name>
-          <Image source={{uri: product?.img}} />
+          {product?.name ? <Name>{product?.name}</Name> : <TitleLoading />}
+
+          {product?.img ? (
+            <Image source={{uri: product?.img}} />
+          ) : (
+            <ImageLoading />
+          )}
 
           <DescriptionContainer>
-            <Price>{product?.price}</Price>
-            <Installment>{product?.installment}</Installment>
-            <PromotionContainer>
-              <PromotionText>{product?.promotion}</PromotionText>
-            </PromotionContainer>
-            <SoldBy>
-              {'Vendido por\n'}
-              <SoldBy>{product?.soldBy}</SoldBy>
-            </SoldBy>
+            {isDescriptionLoaded ? (
+              <>
+                <Price>{product?.price}</Price>
+                <Installment>{product?.installment}</Installment>
+                <PromotionContainer>
+                  <PromotionText>{product?.promotion}</PromotionText>
+                </PromotionContainer>
+                <SoldBy>
+                  {'Vendido por\n'}
+                  <SoldBy>{product?.soldBy}</SoldBy>
+                </SoldBy>
+              </>
+            ) : (
+              <DescriptionsLoading />
+            )}
           </DescriptionContainer>
 
           <FooterContainer>
             <QRCodeContainer>
-              <QRCodeTitle>QR Code para desconto</QRCodeTitle>
-              <QRCodeImage source={{uri: product?.qrCodeImg}} />
+              {product?.qrCodeImg ? (
+                <>
+                  <QRCodeTitle>QR Code para desconto</QRCodeTitle>
+                  <QRCodeImage source={{uri: product?.qrCodeImg}} />
+                </>
+              ) : (
+                <QrCodeLoading />
+              )}
             </QRCodeContainer>
           </FooterContainer>
         </ProductContainerContents>
